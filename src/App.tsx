8db20a6fd54877a40cc1d2edd82f01c7eb796664
rodrigodoprado <rodrigodoprado1992@gmail.com>
2022-10-20 18:19:48 -0300

    primeiro commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Footer from './componete/footer';
+import NavBar from './componete/navBar';
+import About from './pages/about';
+import Login from './pages/auth/login';
+import Register from './pages/auth/register';
+import DashboardIndex from './pages/dashboard';
+import Cadastrar from './pages/dashboard/cadastrar';
+import Perfil from './pages/dashboard/perfil';
+import Filmes from './pages/filmes';
+import Home from './pages/home';
+import Series from './pages/series';
+import View from './pages/view';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      {/* <AuthProvider> */}
+      <BrowserRouter>
+        <Routes>
+          {/* rota public */}
+          <Route path="*" element={<Home />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/filmes" element={<Filmes />} />
+          <Route path="/series" element={<Series />} />
+          <Route path="/produto/:id" element={<View />} />
+          {/* rotas private */}
+          <Route path="/dashboard" element={<DashboardIndex />} />
+          <Route path="/dashboard/produto" element={<Cadastrar />} />
+          <Route path="/dashboard/perfil" element={<Perfil />} />
+        </Routes>
+      </BrowserRouter>
+      {/* </AuthProvider> */}
+      <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
