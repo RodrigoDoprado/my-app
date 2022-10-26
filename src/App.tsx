72130195bd0b4c6be99aa73f1462d1bddeb502ff
rodrigodoprado <rodrigodoprado1992@gmail.com>
2022-10-26 10:49:44 -0300

    remoção da dashboard

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Footer from './componete/footer';
+import NavBar from './componete/navBar';
+import About from './pages/about';
+import Filmes from './pages/filmes';
+import Home from './pages/home';
+import Series from './pages/series';
+import View from './pages/view';
+
+function App() {
+
+  return (
+    <div className="App">
+      <NavBar />
+      {/* <AuthProvider> */}
+      <BrowserRouter>
+        <Routes>
+          {/* rota public */}
+          <Route path="*" element={<Home />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/filmes" element={<Filmes />} />
+          <Route path="/series" element={<Series />} />
+          <Route path="/produto/:id" element={<View />} />
+          
+        </Routes>
+      </BrowserRouter>
+      {/* </AuthProvider> */}
+      <Footer />
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/filmes.tsx
@@ -0,0 +1,23 @@
+import { Helmet } from "react-helmet";
+
+export default function Filmes() {
+    return (
+        <>
+            <Helmet><title>Filmes</title></Helmet>
+            <section className="resume-section">
+                <div className="resume-section-content">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-sm">
+                                <h1>Filmes</h1>
+                            </div>
+                            <div className="col-sm">
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
