2180305a609d856939089a75f236377e01dfeb0a
RodrigoDoprado <68282603+RodrigoDoprado@users.noreply.github.com>
2022-11-01 15:54:56 -0300

    Update Index/ servece

--- src/service/index.ts
+++ src/service/index.ts
@@ -2,5 +2,6 @@ import axios from "axios"
 
 export const Api = axios.create({
     // baseURL: "https://reqres.in/api/",
-    baseURL: "http://localhost:5000/",
+   // baseURL: "http://localhost:5000/",
+baseURL: "https://typebackend.herokuapp.com/",
 }) 
