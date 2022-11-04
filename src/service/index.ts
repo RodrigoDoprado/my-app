601a2465cfa15c17a256eb95bfb0441d72d3a691
rodrigodoprado <rodrigodoprado1992@gmail.com>
2022-11-03 21:46:07 -0300

    variaves de ambiente

--- src/service/index.ts
+++ src/service/index.ts
@@ -1,12 +1,5 @@
 import axios from "axios"
 
 export const Api = axios.create({
-<<<<<<< HEAD
     baseURL: process.env.API_URL,
 }) 
-=======
-    // baseURL: "https://reqres.in/api/",
-   // baseURL: "http://localhost:5000/",
-baseURL: "https://apiback-end.herokuapp.com",
-}) 
->>>>>>> e0151651f4e6f7bbe12e7152075706da43117579
